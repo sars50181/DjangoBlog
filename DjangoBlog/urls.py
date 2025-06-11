7a325c4c4dac1d75dad088705d107206773db546
sars50181 <sars50181@gmail.com>
2025-06-11 12:03:38 +0800

    A | add view function.

--- DjangoBlog/urls.py
+++ DjangoBlog/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from posts.views import index
+from posts.views import about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", index),
+    path("about/", about),
 ]
