3335034ddce52f08edfd100f627f2a2a35e8d874
sars50181 <sars50181@gmail.com>
2025-07-03 10:08:53 +0800

    fix: prepare to deploy.

--- download_db.bat
+++ download_db.bat
@@ -1,2 +1,2 @@
 rm -rf db.sqlite3
-wget —no-check-certificate  https://docs.google.com/uc?export=download&id=1d9rLGdEplwrrfrzDznj88Kbc5ixKc0By -O db.sqlite3
+wget —no-check-certificate  "https://docs.google.com/uc?export=download&id=1d9rLGdEplwrrfrzDznj88Kbc5ixKc0By" -O db.sqlite3
