388160d91503bfc747b7b5f419ddf2949ababfb7
sars50181 <sars50181@gmail.com>
2025-07-17 11:59:37 +0800

    20250717

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -78,16 +78,16 @@ WSGI_APPLICATION = 'DjangoBlog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
 '''
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -101,7 +101,7 @@ DATABASES = {
         }
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
